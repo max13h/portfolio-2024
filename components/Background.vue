c06306486a8ad4844d634f32ba23f82b588047eb
max13h <contact@max13h.fr>
2024-02-02 09:30:56 +0100

    Put background on sleep

--- components/Background.vue
+++ components/Background.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="w-full h-screen absolute top-0 left-0 -z-50 overflow-hidden bg-center bg-cover" :style="backgroundString">
-</div>
+  <!-- <div class="w-full h-screen absolute top-0 left-0 -z-50 overflow-hidden bg-center bg-cover" :style="backgroundString"> -->
+<!-- </div> -->
+    <div>hello</div>
 </template>
 
 <script setup>
 const random = ref({
   rotation: 200
 })
-
-
 
 onMounted(() => {
   useAnime({
