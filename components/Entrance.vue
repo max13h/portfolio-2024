95791d196575b281a1e9aa91bf39304bd5865baa
max13h <contact@max13h.fr>
2024-02-03 19:21:39 +0100

    fix sticky element with absolute position

--- components/Entrance.vue
+++ components/Entrance.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="z-50 w-full h-screen overflow-hidden sticky top-0">
-    <div class="w-full h-full flex">
-      <div class="entrance-line w-[10%] h-full bg-dark"></div>
-      <div class="entrance-line w-[10%] h-full bg-dark"></div>
-      <div class="entrance-line w-[10%] h-full bg-dark"></div>
-      <div class="entrance-line w-[10%] h-full bg-dark"></div>
-      <div class="entrance-line w-[10%] h-full bg-dark"></div>
-      <div class="entrance-line w-[10%] h-full bg-dark"></div>
-      <div class="entrance-line w-[10%] h-full bg-dark"></div>
-      <div class="entrance-line w-[10%] h-full bg-dark"></div>
-      <div class="entrance-line w-[10%] h-full bg-dark"></div>
-      <div class="entrance-line w-[10%] h-full bg-dark"></div>
+  <div class="absolute top-0 left-0 z-50 w-full h-full">
+    <div class="w-full h-screen sticky top-0">
+      <div class="w-full h-full flex">
+        <div class="entrance-line w-[10%] h-full bg-dark"></div>
+        <div class="entrance-line w-[10%] h-full bg-dark"></div>
+        <div class="entrance-line w-[10%] h-full bg-dark"></div>
+        <div class="entrance-line w-[10%] h-full bg-dark"></div>
+        <div class="entrance-line w-[10%] h-full bg-dark"></div>
+        <div class="entrance-line w-[10%] h-full bg-dark"></div>
+        <div class="entrance-line w-[10%] h-full bg-dark"></div>
+        <div class="entrance-line w-[10%] h-full bg-dark"></div>
+        <div class="entrance-line w-[10%] h-full bg-dark"></div>
+        <div class="entrance-line w-[10%] h-full bg-dark"></div>
+      </div>
+      <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <h1 class="text-light text-xl font-light" :class="{'overflow-hidden': hiddenOverflow}">
+          <span class="heading-pop-up inline-block text-center translate-y-full">Portfolio max13h 2024</span>
+        </h1>
+      </div>
     </div>
-    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <h1 class="text-light text-xl font-light" :class="{'overflow-hidden': hiddenOverflow}">
-        <span class="heading-pop-up inline-block text-center translate-y-full">Portfolio max13h 2024</span>
-      </h1>
-    </div>
+
   </div>
 </template>
 
