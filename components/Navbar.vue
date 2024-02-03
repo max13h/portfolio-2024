c50b2bd3a90117398a3c0a54b3a3da8d49eeb11c
max13h <contact@max13h.fr>
2024-02-04 00:04:48 +0100

    fix typo on navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fixed w-full pt-4 pe-8 flex justify-end">
     <NuxtLink to="#aboutme" class="me-4 text-dark transition-all duration-700">
-      aboutme
+      About me
     </NuxtLink>
     <NuxtLink to="#contact" class="transition-all duration-700">
       Contact
