45694e49b6493b10d17738ba9966a41195d003cb
max13h <contact@max13h.fr>
2024-02-03 23:56:33 +0100

    fix heading and styles

--- components/Entrance.vue
+++ components/Entrance.vue
@@ -14,9 +14,9 @@
         <div class="entrance-line w-[10%] h-full bg-dark"></div>
       </div>
       <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <h1 class="text-xl font-light" :class="{'overflow-hidden': hiddenOverflow}">
+        <p class="text-xl font-light" :class="{'overflow-hidden': hiddenOverflow}">
           <span class="text-light heading-pop-up inline-block text-center translate-y-full">Portfolio max13h 2024</span>
-        </h1>
+        </p>
       </div>
     </div>
 
