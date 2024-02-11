ab006783dc3db41884615dfab032a796e2d73e37
max13h <contact@max13h.fr>
2024-02-11 21:30:40 +0100

    add favicon & pinia to package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,24 @@ export default defineNuxtConfig({
     '@nuxt/image',
     'nuxt-icon',
   ],
+  imports: {
+    dirs: ["stores",
+      "types"
+    ]
+  },
+  app: {
+    head: {
+      title: "max13h's Portfolio",
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "description", content: "Welcome to my portfolio ! " },
+        { name: "theme-color", content: "#f7f6f9" }
+      ],
+      link: [
+        { rel: "icon", href: "/favicon.ico" },
+      ]
+    }
+  },
   pinia: {
     autoImports: [
       "defineStore"
